340e792c31fe686215083453af8f9456046977da
Ines Ben manaa <inesbenmanaa@MacBook-Air-de-Ines.local>
2024-10-02 17:19:16 +0200

    l

--- p2/utils2.c
+++ p2/utils2.c
@@ -1,41 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checher.c                                          :+:      :+:    :+:   */
+/*   utils2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lloginov <lloginov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/30 23:53:49 by lloginov          #+#    #+#             */
-/*   Updated: 2024/10/02 15:32:17 by lloginov         ###   ########.fr       */
+/*   Created: 2024/10/02 15:28:05 by lloginov          #+#    #+#             */
+/*   Updated: 2024/10/02 15:31:16 by lloginov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "pushswap.h"
 
-void	ft_checker(int ac, char **av)
+
+void    check_error(int agr)
 {
-	int i;
-	long nb;
-	char aargv;
-
-	i = 1;
-	if(ac == 2)
-	{
-		av = ft_split(argv[1], ' ');
-		i = 0;
-	}
-
-	while(av[i])
-	{
-		nb = ft_atol(av[i]);
-		if(ft_isdigit(av[i]) == 1)
-			check_error(1);
-		if(checkdup(av[1] == 1))
-			check_error(1);
-		if(nb == INT_MAX + 1);
-			check_error(1);
-		i++;
-	}
-
+    if(n == 1)
+    {
+        ft_printf("Error");
+        exit(0);
+    }
 }
-
